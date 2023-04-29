2042a4495abc291b2c83903f470f0c9c5f749254
Yijun <u7564899@anu.edu.au>
2023-04-29 13:41:03 +1000

    UI in progress

--- app/src/main/java/com/example/echo361/CourseMainpageActivity.java
+++ app/src/main/java/com/example/echo361/CourseMainpageActivity.java
@@ -2,7 +2,9 @@ package com.example.echo361;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CourseMainpageActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class CourseMainpageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_mainpage);
+
+        Button toForum = findViewById(R.id.btn_gotoForum);
+        toForum.setOnClickListener(view -> {
+            Intent intent0 = new Intent(CourseMainpageActivity.this, ForumStudentActivity.class);
+            CourseMainpageActivity.this.startActivity(intent0);
+        });
+
+        Button toChat = findViewById(R.id.btn_gotoChat);
+        toChat.setOnClickListener(view -> {
+            Intent intent0 = new Intent(CourseMainpageActivity.this, SearchChatTarget.class);
+            CourseMainpageActivity.this.startActivity(intent0);
+        });
     }
 }
