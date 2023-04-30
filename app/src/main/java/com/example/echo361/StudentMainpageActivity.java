30a3fca9505971550c810541f3f5f54dc05a845f
Yijun <u7564899@anu.edu.au>
2023-04-30 19:55:59 +1000

    UI except chatPage

--- app/src/main/java/com/example/echo361/StudentMainpageActivity.java
+++ app/src/main/java/com/example/echo361/StudentMainpageActivity.java
@@ -16,19 +16,19 @@ public class StudentMainpageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Student student = (Student) intent.getSerializableExtra("Student");
 
-        Button toEnroll = findViewById(R.id.btn_enroll);
+        Button toEnroll = findViewById(R.id.btn_toEnroll);
         toEnroll.setOnClickListener(view -> {
             Intent intent0 = new Intent(StudentMainpageActivity.this, EnrollActivity.class);
             StudentMainpageActivity.this.startActivity(intent0);
         });
 
-        Button toDrop = findViewById(R.id.btn_drop);
+        Button toDrop = findViewById(R.id.btn_toDrop);
         toDrop.setOnClickListener(view -> {
             Intent intent0 = new Intent(StudentMainpageActivity.this, DropActivity.class);
             StudentMainpageActivity.this.startActivity(intent0);
         });
 
-        Button ToMyCourse = findViewById(R.id.btn_myCourse);
+        Button ToMyCourse = findViewById(R.id.btn_toMyCourse);
         ToMyCourse.setOnClickListener(view -> {
             Intent intent0 = new Intent(StudentMainpageActivity.this, MyCourseActivity.class);
             StudentMainpageActivity.this.startActivity(intent0);
