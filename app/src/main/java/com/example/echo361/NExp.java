693105bcc968a551a53ae94c5107f0329663aed1
yitaozhang <u7504766@anu.edu.au>
2023-05-02 15:55:51 +1000

    update name tokenizer and parser

--- app/src/main/java/com/example/echo361/NExp.java
+++ app/src/main/java/com/example/echo361/NExp.java
@@ -8,6 +8,6 @@ package com.example.echo361;
 public abstract class NExp {
 
 	public abstract String show(); // returns parsed expression of the current expression
-//	public abstract int evaluate(); // returns evaluated value of the current expression
+
 	
 }
