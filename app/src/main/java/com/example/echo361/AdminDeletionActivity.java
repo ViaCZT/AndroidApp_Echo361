2041491ba538042322387ef9d1dc04e3e6a8c5aa
yitaozhang <u7504766@anu.edu.au>
2023-05-06 17:11:08 +1000

    added search function in AdminDeletionActivity.java, EnrollActivity.java, and SearchChatTarget.java

--- app/src/main/java/com/example/echo361/AdminDeletionActivity.java
+++ app/src/main/java/com/example/echo361/AdminDeletionActivity.java
@@ -10,5 +10,7 @@ public class AdminDeletionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_deletion);
+
+
     }
 }
