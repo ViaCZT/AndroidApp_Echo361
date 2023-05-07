de242c0bf363079fbddd43bb4bf8689ebbbd7dd7
Yijun <u7564899@anu.edu.au>
2023-05-07 22:40:34 +1000

    UI Change

--- app/src/main/java/com/example/echo361/SearchChatTarget.java
+++ app/src/main/java/com/example/echo361/SearchChatTarget.java
@@ -69,46 +69,6 @@ public class SearchChatTarget extends AppCompatActivity {
         };
         button.setOnClickListener(myListener2);
 
-     /*   这段是想用searchView，还是由你决定怎么写
-        SearchView searchPerson = findViewById(R.id.searchPerson);
-        searchPerson.setQueryHint("Search the person to chat");
-        searchPerson.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                // 在用户提交搜索时回调
-                // 根据搜索关键字进行搜索操作
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // 在搜索关键字改变时回调
-                // 可以根据搜索关键字进行实时搜索操作
-                return false;
-            }
-        });
-
-        String target = searchPerson.getQuery().toString();*/
-
-//       String target = name.getText().toString();
-//        ArrayList<String> arrayList = new ArrayList<>();
-//        Button search = findViewById(R.id.btn_searchChat);
-//        search.setOnClickListener(view -> {
-//            ArrayList<Student> students = (ArrayList<Student>) Student.search(target);
-//            for(Student s:students){
-//                arrayList.add(s.getname());
-//            }
-//        });
-
-//        ListView listView = findViewById(R.id.list_name);
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
-//        listView.setAdapter(arrayAdapter);
-//        listView.setOnItemClickListener((adapterView, view, i, l) -> {
-//            Intent intent = new Intent(SearchChatTarget.this, ChatActivity.class);
-//            String text = arrayList.get(i);
-//            intent.putExtra("targetName",text);
-//        });
-
 
     }
 
