34e26039953aba9a61d8e382c32308cb1b6e33ee
ZetianChen <u7564812@anu.edu.au>
2023-05-10 01:23:02 +1000

    implement course video

--- app/src/main/java/com/example/echo361/LayoutActivity/CourseMainpageActivity.java
+++ app/src/main/java/com/example/echo361/LayoutActivity/CourseMainpageActivity.java
@@ -1,11 +1,12 @@
 package com.example.echo361.LayoutActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.echo361.R;
 
@@ -35,40 +36,8 @@ public class CourseMainpageActivity extends AppCompatActivity {
         });
 
         VideoView videoView = findViewById(R.id.videoView);
-
-//        MediaController mediaController = new MediaController(this);
-//        mediaController.setAnchorView(videoView);
-//        videoView.setMediaController(mediaController);
-//
-//        String path = "file:///android_asset/COMP2100APKGenerationGroupAssignment.mp4";
-//        Uri uri = Uri.parse(path);
-//        videoView.setVideoURI(uri);
-//
-//        videoView.requestFocus();
-//        videoView.start();
-
-        //实例化播放内核
-//        android.media.MediaPlayer mediaPlayer = new android.media.MediaPlayer();
-//        //获得播放源访问入口
-//        AssetManager am = getAssets();
-//        try {
-//            AssetFileDescriptor afd = am.openFd("COMP2100APKGenerationGroupAssignment.mp4");// 注意这里的区别
-//            //给MediaPlayer设置播放源
-//            mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        //设置准备就绪状态监听
-//        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//            @Override
-//            public void onPrepared(MediaPlayer mp) {
-//                // 开始播放
-//                mediaPlayer.start();
-//            }
-//        });
-//        //准备播放
-//        mediaPlayer.prepareAsync();
-
+        videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/raw/video1"));
+        videoView.start();
 
     }
 }
