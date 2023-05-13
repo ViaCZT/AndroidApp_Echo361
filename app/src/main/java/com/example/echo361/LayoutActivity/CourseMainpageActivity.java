36c2982a8faf44d86085b3cb74ff01d3b861e203
yitaozhang <u7504766@anu.edu.au>
2023-05-14 04:30:08 +1000

    put student id and coursename to SearchChatTarget.java

--- app/src/main/java/com/example/echo361/LayoutActivity/CourseMainpageActivity.java
+++ app/src/main/java/com/example/echo361/LayoutActivity/CourseMainpageActivity.java
@@ -1,15 +1,23 @@
 package com.example.echo361.LayoutActivity;
 
+import static com.example.echo361.Search.Search.readCourseDate;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.echo361.Course;
+import com.example.echo361.Database.FirebaseDAOImpl;
 import com.example.echo361.R;
+import com.google.firebase.FirebaseApp;
+
+import java.util.ArrayList;
 
 public class CourseMainpageActivity extends AppCompatActivity {
 
@@ -17,6 +25,25 @@ public class CourseMainpageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_mainpage);
+
+        Intent intent1 = getIntent();
+        String courseName = intent1.getStringExtra("courseName");
+        String student_id = intent1.getStringExtra("student_id");
+
+//        FirebaseApp.initializeApp(getBaseContext());
+//        FirebaseDAOImpl firebaseDAOImpl = FirebaseDAOImpl.getInstance();
+//
+//        ArrayList<Course> courses = new ArrayList<Course>();
+//
+//        ArrayAdapter courseListAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, courses);
+//
+//        readCourseDate(firebaseDAOImpl, courses, courseListAdapter, courseName);
+//
+//        Log.d("Search chat courses2", "courses from function" + courses);
+//
+//        Log.d("Search chat courses2", "courses from function" + courses);
+
+        //
 
         Button toChat = findViewById(R.id.btn_gotoChat);
         Intent intent2 = getIntent();
@@ -27,7 +54,10 @@ public class CourseMainpageActivity extends AppCompatActivity {
             Log.d("123",course_name);
         boolean is_teacher = intent2.getBooleanExtra("is_teacher",false);
         toChat.setOnClickListener(view -> {
-            Intent intent0 = new Intent(CourseMainpageActivity.this, ChatActivity.class);
+//            Intent intent0 = new Intent(CourseMainpageActivity.this, ChatActivity.class);
+            Intent intent0 = new Intent(CourseMainpageActivity.this, SearchChatTarget.class);
+            intent0.putExtra("courseName", courseName);
+            intent0.putExtra("student_id", student_id);
             CourseMainpageActivity.this.startActivity(intent0);
         });
 
